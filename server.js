7112c84e19a2b3abfcaf72f8c8f0b0687db5fab9
julieandersson <juan2301@student.miun.se>
2024-09-29 15:13:31 +0200

    installerat cors, express, mongoose, nodemon, skapar schemat för arbetserfarenheter, inkluderar schemat till databasen samt gör GET route

--- server.js
+++ server.js
@@ -15,10 +15,52 @@ mongoose.connect("mongodb://localhost:27017/DT207G").then(() => {
     console.log("Fel vid anslutning av databasen: " + error);
 })
 
+// Arbetserfarenheter - schema
+const workExperienceSchema = new mongoose.Schema({
+    companyname: {
+        type: String, 
+        required: [true, "Du måste ange ett företagsnamn."]
+    },
+    jobtitle: {
+        type: String,
+        required: [true, "Du måste ange en jobbtitel."]
+    },
+    location: {
+        type: String,
+        required: [true, "Du måste ange en plats."]
+    },
+    startdate: {
+        type: Date,
+        required: [true, "Du måste ange ett startdatum."]
+    },
+    enddate: {
+        type: Date,
+        required: [true, "Du måste ange ett slutdatum."]
+    },
+    description: {
+        type: String,
+        required: [true, "Du måste ange en beskrivning."]
+    }
+});
+
+// Inkluderar schemat till databas med tabell Workexperience
+const Workexperience = mongoose.model("Workexperience", workExperienceSchema);
+
 // Routes
 app.get("/api", async (req, res) => {
     res.json({message: "Välkommen till mitt API"});
 });
+
+// Get-route - Hämtar arbetserfarenheter
+app.get("/workexperiences", async (req, res) => {
+    try {
+        let result = await Workexperience.find({});
+
+        return res.json(result);
+    } catch(error) {
+        return res.status(500).json(error);
+    }
+})
 
 app.listen(port, () => {
     console.log("Server startad på port: " + port);
