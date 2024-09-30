9265c6b3d32d74088e27fec2bd48cbd2c346ff86
julieandersson <juan2301@student.miun.se>
2024-09-30 19:47:06 +0200

    skapar felhantering för GETroute, adderar meddelande för POST route samt skapar PUT route

--- server.js
+++ server.js
@@ -56,6 +56,11 @@ app.get("/workexperience", async (req, res) => {
     try {
         let result = await Workexperience.find({});
 
+        // Om det ej finns någon data/arbetserfarenheter, skriv ut felmeddelande
+        if (result.length === 0) {
+            return res.status(404).json({ message: "Inga arbetserfarenheter hittades." });
+        }
+
         return res.json(result);
     } catch(error) {
         return res.status(500).json(error);
@@ -67,9 +72,33 @@ app.post("/workexperience", async (req, res) => {
     try {
         let result = await Workexperience.create(req.body);
 
-        return res.json(result);
+        return res.json({ message: "Arbetserfarenhet har lagts till", result});
+
     } catch (error) {
         return res.status(400).json(error);
+    }
+});
+
+// PUT-route - Uppdatera arbetserfarenhet 
+app.put("/workexperience/:id", async (req, res) => {
+    try {
+        // Hämtar job-ID från URL-parameter
+        const id = req.params.id;
+
+        // Hämta och uppdatera arbetserfarenhet med angivet ID
+        let result = await Workexperience.findByIdAndUpdate(id, req.body, {
+            new: true, // Returnerar den uppdaterade arbetserfarenheten istället för den gamla
+        });
+
+        // Kontrollerar om arbetserfarenheten med det angivna ID:t hittades
+        if (!result) {
+            return res.status(404).json({ message: "Ingen arbetserfarenhet kunde hittas med det angivna ID:t." });
+        }
+
+        return res.json({ message: "Arbetserfarenhet uppdaterad", result });
+
+    } catch (error) {
+        return res.status(400).json({ message: "Fel vid uppdatering", error });
     }
 });
 
